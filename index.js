5b1fe10f0eb0ba6e22ae3e05b1240436b330f5e5
vaibhav-0902 <vaibhavwaghela6@outlook.com>
2022-10-30 23:28:12 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("bake the cake");
+// console.log("bake the cake");
+let content = document.querySelector('.content');
+let text = document.getElementById('text-box');
+let char = document.getElementById('character');
+let words = document.getElementById('words');
+let lines = document.getElementById('lines');
+let symbols = document.getElementById('symbols');
+
+
+let count = () => {
+    if (text.value.length === 0) {
+        content.style.display = "none";
+    }
+    else {
+        content.style.display = "flex";
+        char.innerHTML = text.value.replace(/ /g, "").length + " characters";
+        words.innerHTML = text.value.trim().split(/\s+/).length + " words";
+        lines.innerHTML = text.value.split("\n").length + " lines";
+        symbols.innerHTML = text.value.split(/[!@#$%^&*+()_={};:'"<>.,?/-]/).length - 1 + " symbols";
+    }
+}
+
+text.addEventListener("input", count);
